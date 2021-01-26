032900a28257a36da6a6ea555559ab79fb641741
WillyJL <willy.leslie@icloud.com>
2021-01-26 08:19:26 +0100

    Remove redundant pathing

--- modules/onupdate.lua
+++ modules/onupdate.lua
@@ -1,14 +1,6 @@
-function fileExists(filename)
-    local f=io.open(filename,"r") if (f~=nil) then io.close(f) return true else return false end
-end
-function getCWD(mod_name)
-    if fileExists("bin/x64/plugins/cyber_engine_tweaks/mods/"..mod_name.."/init.lua") then return "bin/x64/plugins/cyber_engine_tweaks/mods/"..mod_name.."/" elseif fileExists("x64/plugins/cyber_engine_tweaks/mods/"..mod_name.."/init.lua") then return "x64/plugins/cyber_engine_tweaks/mods/"..mod_name.."/" elseif fileExists("plugins/cyber_engine_tweaks/mods/"..mod_name.."/init.lua") then return "plugins/cyber_engine_tweaks/mods/"..mod_name.."/" elseif fileExists("cyber_engine_tweaks/mods/"..mod_name.."/init.lua") then return "cyber_engine_tweaks/mods/"..mod_name.."/" elseif fileExists("mods/"..mod_name.."/init.lua") then return "mods/"..mod_name.."/" elseif  fileExists(mod_name.."/init.lua") then return mod_name.."/" elseif  fileExists("init.lua") then return "" end
-end
-
 
 Str8upOnUpdate = { 
     description = "Str8up OnUpdate Component",
-    rootPath =  getCWD("Str8up Menu"),
     timers = {
         t025s = 0,
         t2s = 0,
