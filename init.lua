4875f452b7d853ded5ecbf5fce99e933a6abfb8d
Willy-JL <willy.leslie@icloud.com>
2021-09-03 21:12:35 +0200

    Bump version

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ end
 
 Str8upMenu = {
     description = "Str8up Menu",
-    version = "2.4h2",
+    version = "2.4h3",
     rootPath =  getCWD("Str8up Menu"),
     drawWindow = false,
     oldDrawWindow = false
